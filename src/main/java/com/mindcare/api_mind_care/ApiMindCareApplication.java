488bac37bb09bf586809f1518aacfbfc9ac5a71b
Jack <ferreira.marlon@live.com>
2026-06-22 13:33:17 -0300

    feat: initial Spring Boot setup with PostgreSQL

---
+++ src/main/java/com/mindcare/api_mind_care/ApiMindCareApplication.java
@@ -0,0 +1,13 @@
+package com.mindcare.api_mind_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMindCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMindCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindcare/api_mind_care/ApiMindCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindcare.api_mind_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMindCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
