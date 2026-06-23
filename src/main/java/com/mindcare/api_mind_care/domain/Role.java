b809c9c5351bb204fc28e7c623fba2933ff75c10
Jack <ferreira.marlon@live.com>
2026-06-23 11:26:34 -0300

    refactor: remove unused Role and Status enums from domain package

--- src/main/java/com/mindcare/api_mind_care/domain/Role.java
+++
@@ -1,6 +0,0 @@
-package com.mindcare.api_mind_care.domain;
-
-public enum Role {
-    PATIENT,
-    PSYCHOLOGIST
-}
--- src/main/java/com/mindcare/api_mind_care/domain/Status.java
+++
@@ -1,8 +0,0 @@
-package com.mindcare.api_mind_care.domain;
-
-public enum Status {
-    PENDING,
-    CONFIRMED,
-    COMPLETED,
-    CANCELLED
-}
